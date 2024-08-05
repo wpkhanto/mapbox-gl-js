c43ccacb1df402da261695465d44bc75c4949c97
wpkhanto <wpkhanto@icloud.com>
2024-08-05 16:06:14 +0700

    initialize pin from locations data

--- script.js
+++ script.js
@@ -5,7 +5,43 @@ async function loadJSON() {
             throw new Error(`Network respponse was not ok ${response.statusText}`);
         }
         const data = await response.json();
-        console.log(data);
+
+        const bounds = new mapboxgl.LngLatBounds();
+
+        data.data.data[6].locations.forEach(loc => {
+            // Create marker
+            const el = document.createElement('div');
+            el.className = 'marker';
+
+            // Add marker
+            new mapboxgl.Marker({
+                    element: el,
+                    anchor: 'bottom'
+                })
+                .setLngLat(loc.coordinates)
+                .addTo(map);
+
+            // Add popup
+            new mapboxgl.Popup({
+                    offset: 30
+                })
+                .setLngLat(loc.coordinates)
+                .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+                .addTo(map);
+
+            // Extend map bounds to include current location
+            bounds.extend(loc.coordinates);
+        });
+
+        map.fitBounds(bounds, {
+            padding: {
+                top: 200,
+                bottom: 150,
+                left: 100,
+                right: 100
+            }
+        });
+
     } catch (error) {
         console.error('Error fetching the JSON data:', error)
     }
@@ -17,8 +53,8 @@ mapboxgl.accessToken = 'my-token';
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/standard',
-    center: [-118.113491, 34.111745], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-    zoom: 9 // starting zoom
+    // center: [-118.113491, 34.111745], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    // zoom: 9 // starting zoom
 });
 
 
